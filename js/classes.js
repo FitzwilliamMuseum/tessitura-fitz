cc46c6c99d6c3fb48dd7528e0dc069089b3b632e
Daniel Pett <dejp3@cam.ac.uk>
2020-01-29 14:04:56 +0000

    Add classes

--- js/classes.js
+++ js/classes.js
@@ -1,3 +1,5 @@
 $("#tn-subnav-promo-button").addClass("btn-wine");
 $(".tn-additional-events__continue-shopping-link").addClass("btn-wine");
 $(".tn-link").addClass("btn-wine");
+$("#tn-subnav-promo-code").addClass("form-control");
+$("#tn-apply-promo").addClass("form-inline");
