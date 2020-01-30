70255107f77bb2d180c1eea1ac0e2487cb713592
Daniel Pett <dejp3@cam.ac.uk>
2020-01-30 12:12:32 +0000

    Count timer

--- js/classes.js
+++ js/classes.js
@@ -3,5 +3,6 @@ $(".tn-cart-buttons__secondary-action").addClass("btn-wine");
 $(".tn-additional-events__continue-shopping-link").addClass("btn-wine");
 $(".tn-additional-events__production-season-detail-link").addClass("btn-wine");
 $(".tn-link").addClass("btn-wine");
+$(".tn-timer").addClass("btn-wine");
 $("#tn-subnav-promo-code").addClass("form-control");
 $("#tn-apply-promo").addClass("form-inline");
