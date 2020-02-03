e74686ffa28a4db593f4d193063d15073d7d83fa
Daniel Pett <dejp3@cam.ac.uk>
2020-02-03 10:54:59 +0000

    Add classes

--- js/classes.js
+++ js/classes.js
@@ -1,10 +1,10 @@
-$("#tn-subnav-promo-button").addClass("btn-wine");
-$(".tn-cart-buttons__secondary-action").addClass("btn-wine");
-$(".tn-additional-events__continue-shopping-link").addClass("btn-wine");
-$(".tn-additional-events__production-season-detail-link").addClass("btn-wine");
-$(".tn-link").addClass("btn-wine");
-$(".tn-timer").addClass("btn-wine");
-$("a.btn.btn-info.btn-block.tn-register-button").addClass("btn-wine");
+$("#tn-subnav-promo-button").addClass("btn-wine").addClass("btn-lg");
+$(".tn-cart-buttons__secondary-action").addClass("btn-wine").addClass("btn-lg");
+$(".tn-additional-events__continue-shopping-link").addClass("btn-wine").addClass("btn-lg");
+$(".tn-additional-events__production-season-detail-link").addClass("btn-wine").addClass("btn-lg");
+$(".tn-link").addClass("btn-wine").addClass("btn-lg");
+$(".tn-timer").addClass("btn-wine").addClass("btn-lg");
+$("a.btn.btn-info.btn-block.tn-register-button").addClass("btn-wine").addClass("btn-lg");
 $("#tn-subnav-promo-code").addClass("form-control");
 $("#tn-apply-promo").addClass("form-inline");
 $(".tn-event-detail__unavailable-text").addClass("btn-wine").addClass("btn-lg");
