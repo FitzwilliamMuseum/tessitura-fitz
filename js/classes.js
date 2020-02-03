28324c069e718d0059a71e2ce1f675b1958ad7df
Daniel Pett <dejp3@cam.ac.uk>
2020-02-03 13:37:40 +0000

    Meh

--- js/classes.js
+++ js/classes.js
@@ -9,4 +9,4 @@ $("#tn-subnav-promo-code").addClass("form-control");
 $("#tn-apply-promo").addClass("form-inline");
 $(".tn-event-detail__unavailable-text").addClass("btn-wine btn-lg");
 $(".tn-prod-season-header__image").addClass("rounded-circle");
-$('.tn-prod-list-item__property--img-container').children('img').addClass("rounded-circle");
+$('.tn-prod-list-item__property--img-container').addClass("rounded-circle");
