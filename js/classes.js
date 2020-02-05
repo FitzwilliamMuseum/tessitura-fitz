e9e18494b47df282ccdb2b69fb3231cf8ce41108
Daniel Pett <dejp3@cam.ac.uk>
2020-02-05 15:21:12 +0000

    Remove and add classes for login

--- js/classes.js
+++ js/classes.js
@@ -9,3 +9,5 @@ $('#tn-subnav-promo-code').addClass('form-control');
 $('#tn-apply-promo').addClass('form-inline');
 $('.tn-event-detail__unavailable-text').addClass('btn-wine btn-lg');
 $('.tn-prod-season-header__image').addClass('rounded-circle');
+$('.col-xs-12').addClass('col-lg-12').removeClass('col-xs-12');
+$('.col-sm-6').removeClass('col-sm-6');
