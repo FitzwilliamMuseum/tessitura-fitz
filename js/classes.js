b07c3cf3411781d29295c051ab8680825d531c2a
Daniel Pett <dejp3@cam.ac.uk>
2020-07-22 12:46:43 +0100

    Add container

--- js/classes.js
+++ js/classes.js
@@ -23,3 +23,4 @@ $('.col-sm-push-8').removeClass('col-sm-push-8');
 $('.col-sm-pull-2').removeClass('col-sm-pull-2');
 $('.col-sm-push-10').removeClass('col-sm-push-10');
 $('.tn-detail-production-text-toggle-link').addClass('btn-dark').removeClass('btn-default');
+$('.tn-prod-season-header').addClass('container');
