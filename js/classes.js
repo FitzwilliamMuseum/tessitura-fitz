76543d47154997c5426c0335e144e3fc77a94a7d
Daniel Pett <dejp3@cam.ac.uk>
2021-05-14 23:28:25 +0100

    Update classes.js

--- js/classes.js
+++ js/classes.js
@@ -51,4 +51,5 @@ $("h4.tn-ticket-selector__pricetype-name").text(function () {
     return $(this).text().replace("£0.00", " FREE"); 
 });
 $(".tn-interests label").text().slice(3);
-$( "tn-interest-3-label" ).trim().text().slice(3);
+var tn3 = $.trim($( "tn-interest-3-label" ).text());
+tn3.slice(3);
