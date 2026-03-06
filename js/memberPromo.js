ff3d465178c5af6a77559257c871c564860d2f53
Chris Butterworth <chris@studio24.net>
2026-03-06 12:29:06 +0000

    Updating member promo when multiple slots are available

--- js/memberPromo.js
+++ js/memberPromo.js
@@ -1,22 +1,24 @@
 function memberPromo() {
     const message = '<a href="https://tickets.museums.cam.ac.uk/account/login?returnurl=%2fevents">Sign in</a> as a Friend and book your free tickets. Not yet a member? <a href="https://www.fitzmuseum.cam.ac.uk/support-us/become-a-friend">Join today</a>';
 
-
-    const heading = document.querySelector('.h3.tn-event-detail__ticket-selection-section-heading.tn-event-detail__ticket-selection-section-heading--ticket-selector');
-
-    if (!heading) {
-        return;
-    }
-
-    if (heading.textContent.replace(/\s+/g, '') === '') {
-        heading.textContent = 'Tickets';
-    }
+    const headings = document.querySelectorAll('.h3.tn-event-detail__ticket-selection-section-heading.tn-event-detail__ticket-selection-section-heading--ticket-selector');
 
     const p = document.createElement('p');
     p.className = 'member-promo';
     p.innerHTML = message;
 
-    heading.after(p);
+    
+    [...headings].forEach(heading => {
+        if (heading.textContent.replace(/\s+/g, '') === '') {
+            heading.textContent = 'Tickets';
+        }
+
+        const p = document.createElement('p');
+        p.className = 'member-promo';
+        p.innerHTML = message;
+
+        heading.after(p);
+    });
 
 }
 
